bc0cd7392adfc6a3ba062110aebb26735670fcaa
admin <>
2019-08-19 20:02:22 +0800

    xcodeproject init

---
+++ com.fastLane.test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  com.fastLane.test
+//
+//  Created by admin on 2019/8/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ com.fastLane.test/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  com.fastLane.test
+//
+//  Created by admin on 2019/8/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
