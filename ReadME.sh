6fe9f845affe3808ba19adffda0f7d7cd3dbde6e
admin <>
2019-08-19 20:38:19 +0800

    fastlane init

---
+++ ReadME.sh
@@ -0,0 +1,15 @@
+#超级签名  https://juejin.im/post/5cdeb72151882525cc707729
+#fasetlane  https://www.jianshu.com/p/19ae8cc865b0
+
+#install
+sudo gem install fastlane
+
+#Fastlane bundle update卡主解决方案 先替换 GEM 文件 佛祖删除重装
+https://www.jianshu.com/p/7f9c7168eb5e
+
+#cd pwd 
+fastlane init
+
+gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
+
+
